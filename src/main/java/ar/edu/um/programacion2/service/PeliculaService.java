b28b43ca5880f68ae466ea06c4652617d2e539ce
Franco Ayala <frma98@gmail.com>
2020-12-02 16:15:07 -0300

    Arreglado error de extremos en obtener disponibilidades

--- src/main/java/ar/edu/um/programacion2/service/PeliculaService.java
+++ src/main/java/ar/edu/um/programacion2/service/PeliculaService.java
@@ -26,11 +26,12 @@ public class PeliculaService {
 			throws NoSuchElementException {
 		Pelicula pelicula = peliculaRespository.findById(id).orElseThrow();
 		Stream<LocalDate> fechasDisponibles = inicio.datesUntil(fin.plusDays(1));
-		// List<LocalDate> fechas = secuenciaDeFechas.collect(Collectors.toList());
 		List<LocalDate> fechas = fechasDisponibles.filter(new Predicate<LocalDate>() {
 			@Override
 			public boolean test(LocalDate t) {
-				return t.isBefore(pelicula.getFechaFin()) && t.isAfter(pelicula.getFechaInicio());
+				LocalDate fin = pelicula.getFechaFin().plusDays(1);
+				LocalDate inicio = pelicula.getFechaInicio().minusDays(1);
+				return t.isBefore(fin) && t.isAfter(inicio);
 			}
 		}).collect(Collectors.toList());
 		return new PeliculaDisponibilidadesDTO(pelicula, fechas);
