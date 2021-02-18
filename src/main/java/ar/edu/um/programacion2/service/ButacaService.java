dd3f438ddb99681890021123ce3235d39725ef8d
Franco Ayala <frma98@gmail.com>
2021-02-18 12:24:41 -0300

    Saque comentarios, funciones que no usaba y cambie anotaciones.

--- src/main/java/ar/edu/um/programacion2/service/ButacaService.java
+++ src/main/java/ar/edu/um/programacion2/service/ButacaService.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneId;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ar.edu.um.programacion2.domain.Butaca;
@@ -20,23 +21,14 @@ public class ButacaService {
 
 	private static final String ENTITY_NAME = "butaca";
 	
-	private final ButacaRepository butacaRepository;
-	private final ProyeccionRepository proyeccionRepository;
-	private final PeliculaRepository peliculaRepository;
-	
-	public ButacaService(PeliculaRepository peliculaRepository, ProyeccionRepository proyeccionRepository,
-						ButacaRepository butacaRepository) {
-		this.butacaRepository = butacaRepository;
-		this.peliculaRepository = peliculaRepository;
-		this.proyeccionRepository = proyeccionRepository;
-	}
+	@Autowired
+	private ButacaRepository butacaRepository;
+	@Autowired
+	private ProyeccionRepository proyeccionRepository;
 	
 	public Butaca save(Butaca butaca) throws BadRequestAlertException {
 		Proyeccion proyeccion = proyeccionRepository.findById(butaca.getProyeccion().getId()).get();
 		Pelicula pelicula = proyeccion.getPelicula();
-		/*Boolean isButacaBetween = isDateBetween(butaca.getFechaDeVenta(), 
-												LocalDateTime.of(pelicula.getFechaInicio(), LocalTime.MAX), 
-												LocalDateTime.of(pelicula.getFechaFin(), LocalTime.MAX));*/
 		if(butaca.getFechaDeVenta().isEqual(LocalDate.ofInstant(proyeccion.getHora(), ZoneId.systemDefault()))) {
 			throw new BadRequestAlertException("Can't make this today", ENTITY_NAME, "BadDate");
 		}
@@ -45,9 +37,4 @@ public class ButacaService {
 		}
 		return butacaRepository.save(butaca);
 	}
-	
-	private Boolean isDateBetween(LocalDateTime date, LocalDateTime beggining, LocalDateTime end) {
-		return date.isBefore(end) && date.isAfter(beggining);
-	}
-	
 }
